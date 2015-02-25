fc3ae0d199027f0163a04cf79817dc1bdf804a4b
sasiek <martinl@op.pl>
2015-02-25 21:16:16 +0000

    library module

--- js/library.js
+++ js/library.js
@@ -1,6 +1,24 @@
 angular.module("library", ["firebase", "ui.router"])
     .constant('FIREBASE_URL','https://libraryzti.firebaseio.com')
 
-    .config(
-        )
+    .config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('book', {
+                url: '/book',
+                templateUrl: 'templates/book.html',
+                controller: 'bookCtrl'
+        })
+            .state('books', {
+                url: '/books',
+                templateUrl: 'templates/books.html',
+                controller: 'booksCtrl'
+        })            
+            .state('category', {
+                url: '/category',
+                templateUrl: 'templates/category.html',
+                controller: 'categoryCtrl'
+        })            
+  $urlRouterProvider.otherwise('/books');
+})
     
+   
